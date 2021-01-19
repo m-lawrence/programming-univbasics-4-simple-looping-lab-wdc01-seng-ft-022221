820d278409bfa3576bfb8578f8698e06939070ac
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-19 19:04:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,11 @@
 def loop_message_five_times(string)
-  puts 5 * string 
+  puts string
+  puts string 
+  puts string
+  puts string
+  puts string
 end
 
 def loop_message_n_times(string, integer)
-  
+  puts string * integer
 end 
