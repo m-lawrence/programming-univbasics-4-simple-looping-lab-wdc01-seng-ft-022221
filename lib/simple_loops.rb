a9d33cf4560488feb805bd79364bd2976e0da9b8
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-19 19:07:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,11 @@ end
 def loop_message_n_times(string, integer)
   integer.times{puts string}
 end 
+
+def output_array(array)
+  i = 0 
+  while i < array.length do
+    puts array[i]
+    i += 1 
+  end
+end 
